b86f7fe66992dc45173ff005d8c8fa7c001361bb
marcor2742 <marcoruggiero.mail@gmail.com>
2024-12-16 00:50:56 +0100

    sito statico base

---
+++ srcs/requirements/bonus/static/tools/www/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const welcomeButton = document.getElementById('welcomeButton');
+    welcomeButton.addEventListener('click', () => {
+        alert('Welcome to my resume website!');
+    });
+});
